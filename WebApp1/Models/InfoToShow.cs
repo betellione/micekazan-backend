1cce5b7bf7c2d1de8f933527c548a3324a999e9b
Тимур <timurmiftyakov@gmail.com>
2023-11-21 17:57:19 +0300

    fix

--- WebApp1/Models/InfoToShow.cs
+++ WebApp1/Models/InfoToShow.cs
@@ -3,11 +3,11 @@
 public class InfoToShow
 {
     public long Id { get; set; }
-    public string Email { get; set; } = null!;
-    public string Phone { get; set; } = null!;
-    public string ClientName { get; set; } = null!;
-    public string ClientSurname { get; set; } = null!;
-    public string ClientMiddleName { get; set; } = null!;
-    public string OrganizationName { get; set; } = null!;
-    public string WorkPosition { get; set; } = null!;
+    public string? Email { get; set; } = null!;
+    public string? Phone { get; set; } = null!;
+    public string? ClientName { get; set; } = null!;
+    public string? ClientSurname { get; set; } = null!;
+    public string? ClientMiddleName { get; set; } = null!;
+    public string? OrganizationName { get; set; } = null!;
+    public string? WorkPosition { get; set; } = null!;
 }
