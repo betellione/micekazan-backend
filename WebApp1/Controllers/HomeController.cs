21cb8dbd8c261f6b7d211c8ef1fdf5be807c7f3b
Тимур <timurmiftyakov@gmail.com>
2023-12-08 03:58:00 +0300

    terminal

--- WebApp1/Controllers/HomeController.cs
+++ WebApp1/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebApp1.Data.Stores;
+using WebApp1.Enums;
 using WebApp1.Models;
 using WebApp1.Services.EventService;
+using WebApp1.ViewModels.Event;
 using WebApp1.ViewModels.Home;
 
 namespace WebApp1.Controllers;
@@ -12,12 +15,16 @@ public class HomeController : Controller
     private readonly IEventService _eventService;
     private readonly SignInManager<User> _signInManager;
     private readonly UserManager<User> _userManager;
+    private readonly IScreenStore _screenStore;
+    private readonly IScannerStore _scannerStore;
 
-    public HomeController(IEventService eventService, SignInManager<User> signInManager, UserManager<User> userManager)
+    public HomeController(IEventService eventService, SignInManager<User> signInManager, UserManager<User> userManager, IScreenStore screenStore, IScannerStore scannerStore)
     {
         _eventService = eventService;
         _signInManager = signInManager;
         _userManager = userManager;
+        _screenStore = screenStore;
+        _scannerStore = scannerStore;
     }
 
     [HttpGet]
@@ -25,6 +32,8 @@ public class HomeController : Controller
     {
         if (!_signInManager.IsSignedIn(User)) return RedirectToAction("Login", "Account");
 
+        if (User.Claims.Any(x => x is { Type: ClaimTypes.Actor, Value: "Automate" })) return RedirectToAction("Terminal", "Home");
+        
         return User.Claims.First(x => x.Type == ClaimTypes.Role).Value switch
         {
             "Organizer" => RedirectToAction("Index", "Event"),
@@ -36,5 +45,20 @@ public class HomeController : Controller
             }),
             _ => _signInManager.IsSignedIn(User) ? View() : RedirectToAction("Login", "Account"),
         };
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Terminal()
+    {
+        var scanner = await _scannerStore.FindScannerById(new Guid(_userManager.GetUserId(User)!));
+        if (scanner is null) return NotFound();
+        var vm = await _screenStore.GetScreenByType(scanner.EventId, ScreenTypes.Waiting);
+        if (vm is null) return View(new ScreenViewModel());
+        return View(new ScreenViewModel
+        {
+            MainText = vm.WelcomeText,
+            Description = vm.Description,
+            BackgroundColor = vm.TextColor,
+        });
     }
 }
